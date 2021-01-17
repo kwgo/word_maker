a8a14a7f7de1c0be288d9de9516bcd3050483bf3
Jiang Chang <jiangchang@gmail.com>
2021-01-17 12:01:42 -0500

    Initial Commit

---
+++ sokoban-watch WatchKit Extension/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  sokoban-watch WatchKit Extension
+//
+//  Created by Rui Chang on 2021-01-17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ sokoban-watch WatchKit Extension/sokoban_watchApp.swift
@@ -0,0 +1,19 @@
+//
+//  sokoban_watchApp.swift
+//  sokoban-watch WatchKit Extension
+//
+//  Created by Rui Chang on 2021-01-17.
+//
+
+import SwiftUI
+
+@main
+struct sokoban_watchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            NavigationView {
+                ContentView()
+            }
+        }
+    }
+}
