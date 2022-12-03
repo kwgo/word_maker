b8e50fdbfcb416bca3c0b447c8de85093255349d
jiangchang <jiangchang@gmail.com>
2022-12-03 11:17:55 -0500

    add fade in

--- app/src/main/java/com/jchip/word/maker/viewer/MainView.java
+++ app/src/main/java/com/jchip/word/maker/viewer/MainView.java
@@ -2,16 +2,30 @@ package com.jchip.word.maker.viewer;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.view.View;
+import android.view.animation.AccelerateInterpolator;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
+import android.view.animation.AnimationSet;
+import android.view.animation.DecelerateInterpolator;
 
 import com.jchip.word.maker.GameActivity;
+import com.jchip.word.maker.R;
 
 public class MainView {
     private Activity activity;
-    private WordView wordView;
+
     public MainView(Activity activity) {
         this.activity = activity;
 
-        this.wordView = new WordView(activity, "3456789", 48, this::onAction);
+        new WordView(activity, "3456789", 48, this::onAction);
+
+        Animation fadeIn = new AlphaAnimation(0, 1);
+        fadeIn.setInterpolator(new DecelerateInterpolator());
+        fadeIn.setDuration(5000);
+
+        View view = activity.findViewById(R.id.view_word);
+        view.setAnimation(fadeIn);
     }
 
     private void onAction(int index, String letter) {
